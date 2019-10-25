5aa57825989e7be8cf354aa46c01494a59c19820
Adgbd <1403329865@qq.com>
2019-10-25 22:14:30 +0800

    登录功能基本实现

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div id="app">
-    <el-button type="success">成功按钮</el-button>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  components: {}
+  name: 'app'
 }
 </script>
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,61 @@
 <template>
-  <div></div>
+  <div class="bgc">
+    <el-card>
+      <img src="../../assets/logo_index.png" alt />
+      <el-form ref="form">
+        <el-form-item>
+          <el-input v-model="form.name" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="form.verification"
+            placeholder="验证码"
+            style="width:65%; margin-right:10px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true" aria-checked>备选项</el-checkbox>
+          <el-button type="primary" style="width: 100%;">立即创建</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      form: {
+        name: '',
+        verification: ''
+      }
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="less">
+.bgc {
+  width: 100%;
+  height: 100%;
+  background: url(../../assets/login_bg.jpg) no-repeat center / cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  .el-card {
+    width: 400px;
+    height: 350px;
+    position: relative;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+
+    img {
+      display: block;
+      margin: 0 auto 30px;
+    }
+  }
+}
 </style>
